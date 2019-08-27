62d491da57e9ddacffeb7c2ac234abd11507901b
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-27 11:55:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,11 +7,11 @@ class Person
     @name
   end
   
-  def jobs=(persons_job)
-    @jobs = persons_job
+  def job=(persons_job)
+    @job = persons_job
   end
   
-  def jobs
-    @jobs
+  def job
+    @job
   end
 end
